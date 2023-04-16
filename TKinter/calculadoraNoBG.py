9d8849dcec72726a9ac9f554c893f0da5b710b60
Liuflee <liuflee@outlook.com>
2023-04-16 01:01:22 -0400

    343

--- TKinter/calculadoraNoBG.py
+++ TKinter/calculadoraNoBG.py
@@ -1,11 +1,18 @@
+
+#Importar bibliotecas y funciones, tkinter - interfaz, random, imagetk para el bg, messagebox para el mensaje de error
 import tkinter as tk
 import random
-from PIL import Image, ImageTk
+import tkinter.messagebox as mbox
+
+#Creación ventana principal de tk
+ventana = tk.Tk()
+ventana.geometry("250x250")
+
+#Random entre 1 y 6
 
 azar = random.randint(1, 6)
 
-ventana = tk.Tk()
-ventana.geometry("250x250")
+#Titulos aleatorios para la ventana segun la funcion de arriba
 
 if azar == 1:
     ventana.title("Epic Calculator")
@@ -17,6 +24,8 @@ elif azar==5:
     ventana.title("Epic Calculator: ¡Prueba el té de burbujas!")
 else:
     ventana.title("Eso Tilin")
+
+#definicion de las funciones para las operaciones matematicas
 
 def suma():
     resultado = float(num1.get()) + float(num2.get())
@@ -34,10 +43,20 @@ def multiplicación():
     resultado = float(num1.get()) * float(num2.get())
     texto.config(text=f"El resultado es {resultado}")
 
-def potencia():
-    resultado = float(num1.get()) ** float(num2.get())
-    texto.config(text=f"El resultado es {resultado}")
+#La funcion de potencia tiene un mensaje de error si el resultado es demasiado grande
 
+def potencia():
+    try:
+        resultado = float(num1.get()) ** float(num2.get())
+        if abs(resultado) > 1e12:
+            raise ValueError("Resultado demasiado grande para mostrar")
+        texto.config(text=f"El resultado es {resultado}")
+    except Exception as e:
+        texto.config(text="")
+        mbox.showerror("Error", "Resultado demasiado grande para mostrar")
+
+
+#menú desplegable con las opciones de operación
 marco1 = tk.Frame(ventana)
 marco1.place(x=170, y=100)
 
@@ -48,6 +67,8 @@ opcion_seleccionada.set(operaciones[0])
 menu = tk.OptionMenu(marco1, opcion_seleccionada, *operaciones)
 menu.pack()
 
+#entradas para los numeros en la ventana
+
 num1 = tk.Entry(ventana)
 num1.pack()
 num1.place(x=30, y=100)
@@ -56,20 +77,26 @@ num2 = tk.Entry(ventana)
 num2.pack()
 num2.place(x=30, y=120)
 
+#titulo dentro de la ventana
+
 titulo =tk.Label(text="Bienvenid@ a la calculadora\nepica de Anette")
 titulo.pack()
 titulo.place(x=50, y=30)
 
+#texto que da la operación, vacio por ahora
 
 texto = tk.Label(ventana, text="")
 texto.pack()
 texto.place(x=70, y=150)
 
+#boton para calcular
+
 boton = tk.Button(ventana, text="Calcular")
 boton.pack()
 boton.place(x=100, y=180)
-
 boton.config(command=suma)
+
+#funcion que cambia lo que hace el boton segun lo seleccionado en el menú
 
 def cambiar_funcion(*args):
     if opcion_seleccionada.get() == "Suma":
@@ -84,5 +111,8 @@ def cambiar_funcion(*args):
         boton.config(command=potencia)
 
 opcion_seleccionada.trace("w", cambiar_funcion)
+
+
+#loop principal que ejcuta la ventana
 
 ventana.mainloop()
