8fde3742d1e5072d052c5c73f12f1fbad79e5d82
Anette Villalón <liuflee@outlook.com>
2023-04-28 13:18:10 -0400

    a

---
+++ Practica/Try_Except/Ejercicio1.py
@@ -0,0 +1,31 @@
+'''Ejercicio 1
+Considere el caso anterior y aplique:
+Try y except donde corresponda
+En la opción 1, indique si un número ingresado es par o impar
+En la opción 2, muestre la serie 
+Fibonacci de los primeros 10 números
+La opción 3, es Salir de la aplicación'''
+
+while True:
+    try:
+        opcion = int(input("\nElija una opción:\n1 - Saber si un numero es par o impar\n2 - Fibonacci\n3 - Salir\n"))
+        
+        if opcion == 1:
+            num = int(input("\nIngrese un numero: "))
+            if num % 2 == 0:
+                print("El numero ingresado es par\n")
+            else:
+                print("El numero es impar\n")
+        elif opcion == 2:
+            fibonacci = [0, 1] 
+            for i in range(2, 10): 
+                fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+            print(fibonacci)
+
+        elif opcion == 3:
+            print("\nHasta luego!")
+            break
+        else:
+            print("\nOpción no válida\n")
+    except ValueError:
+        print("El valor ingresado no es valido")
