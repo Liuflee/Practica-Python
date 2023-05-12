46254e1f18bcdb19f1ceb072a5b329db08781124
Anette Villalón <127684512+Liuflee@users.noreply.github.com>
2023-05-12 10:13:02 -0400

    a
    a

---
+++ Ejercicio 13 Remake.py
@@ -0,0 +1,37 @@
+'''Desarrolle un algoritmo que permita ingresar notas y sus respectivas ponderaciones
+finalizado el ingreso calcule el promedio.
+Realice las validaciones necesarias.'''
+
+promedio = 0
+suma_ponderacion = 0
+
+while True:
+    try:
+        nota = float(input("Ingrese una nota entre 1 y 7: "))
+    except ValueError:
+        print("Solo se deben ingresar numeros")
+        continue
+
+    while True:
+
+        if 1 <= nota <= 7:
+            try:
+                ponderacion = int(input("Ingrese la ponderación de la nota sin (%): "))
+                if suma_ponderacion + ponderacion > 100:
+                    print(f"La suma de las ponderaciones no puede ser mayor a 100. La actual es: {suma_ponderacion}")
+                elif 0 < ponderacion <= 100:
+                    suma_ponderacion += ponderacion
+                    promedio += nota * ponderacion
+                    break
+
+            except ValueError:
+                print("Solo se deben ingresar numeros enteros para la ponderación")
+    
+    if suma_ponderacion == 100:
+        break
+
+if suma_ponderacion == 0:
+    print("No se ingresaron notas")
+else:
+    promedio /= suma_ponderacion
+    print(f"El promedio ponderado es: {promedio:.2f}")
