72a459f0ba572494eede6fce2f21c74ee56b2f39
Anette Villalón <liuflee@outlook.com>
2023-05-14 13:58:54 -0400

    u

--- ejercicio10.py
+++ ejercicio10.py
@@ -11,30 +11,45 @@ iv. Si ambos son impares'''
 while True:
     try:
         num1 = int(input("Ingrese el primer numero: "))
-        num2 = int (input("Ingrese el segundo numero: "))
+        if num1 == 0:
+            raise ZeroDivisionError("El primer numero no puede ser cero")
+        while True:
+            try:
+                num2 = int(input("Ingrese el segundo numero: "))
+                if num2 == 0:
+                    raise ZeroDivisionError("El segundo numero no puede ser cero")
+                else:
+                    break
+            except ValueError:
+                print("Error: Debe ingresar numeros enteros que no sean 0")
+                continue
+            except ZeroDivisionError as e:
+                print("Error", e)
+                continue
     except ValueError:
-        print("Debe ingresar 2 numeros enteros que no sean 0")
+        print("Error: Debe ingresar numeros enteros que no sean 0")
+    except ZeroDivisionError as e:
+        print("Error:", e)
+
     else:
-        if num1 != 0 and num2 != 0:
-            if num1 % num2 == 0:
-                print("El numero 1 es divisible por el segundo numero")
-            else:
-                print("El numero 1 NO es divisble por el segundo")
-
-            if num2 % num1 == 0:
-                print("El numero 2 es divisible por el primer numero")
-            else:
-                print("El numero 2 NO es divisible por el primer numero")
-
-            if num1 < num2:
-                print("El numero 1 es menor que el numero 2")
-            elif num1 == num2:
-                print("Ambos numeros son iguales")
-            else:
-                print("El numero 2 es menor que el numero 1")
-
-            if not (num1 % 2 == 0) and not (num2 % 2 == 0):
-                print("Ambos numeros son impares")
-            break
+        if num1 % num2 == 0:
+            print("El numero 1 es divisible por el segundo numero")
         else:
-            print("Los numeros no deben ser 0")
+            print("El numero 1 NO es divisble por el segundo")
+
+        if num2 % num1 == 0:
+            print("El numero 2 es divisible por el primer numero")
+        else:
+            print("El numero 2 NO es divisible por el primer numero")
+
+        if num1 < num2:
+            print("El numero 1 es menor que el numero 2")
+        elif num1 == num2:
+            print("Ambos numeros son iguales")
+        else:
+            print("El numero 2 es menor que el numero 1")
+
+        if not (num1 % 2 == 0) and not (num2 % 2 == 0):
+            print("Ambos numeros son impares")
+        break
+
